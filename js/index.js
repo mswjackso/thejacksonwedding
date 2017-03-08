5756f21530a386a54fc6031157eec591f2015d99
Jackson <msjackso@amazon.com>
2017-03-07 22:02:55 -0800

    Added some content to the different sections, fixed spilling over on the map in the location page, added concept of sub pages, added drop down menu

--- js/index.js
+++ js/index.js
@@ -2,17 +2,32 @@
 // When the DOM is fully loaded
 $(document).ready(function() {
 
-  // Set up listener for window size
-  // function onResize() {
-  //   if ($(window).width() < 625) {
-  //     $('#navbar').attr('id', 'mobileNavbar');
-  //   } else {
-  //     $('#mobileNavbar').attr('id', 'navbar');
-  //   }
-  // }
-  // $(window).resize(onResize);
-  // onResize();
+  // Update the count down every 1 second
+  var countDownDate = new Date("Nov 12, 2017 00:00:00");
+  var x = setInterval(function() {
 
+    // Get todays date and time
+    var now = new Date().getTime();
+
+    // Find the distance between now an the count down date
+    var distance = countDownDate - now;
+
+    // Time calculations for days, hours, minutes and seconds
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    // Display the result in the element with id="demo"
+    document.getElementById("titleCountdown").innerHTML = days + "d " + hours + "h "
+    + minutes + "m " + seconds + "s ";
+
+    // If the count down is finished, write some text
+    if (distance < 0) {
+      clearInterval(x);
+      document.getElementById("demo").innerHTML = "EXPIRED";
+    }
+  }, 1000);
 
   // Set up listeners for the desktop navbar
   $('.navlink').click(function() {
